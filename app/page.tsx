f3a0c2176053e39106964d2062b00aec02071169
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-03-17 13:31:50 +0000

    feat: added buttons variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>Hello Lingo</div>
+    <div className='flex flex-col max-w-[200px] gap-y-4 p-4'>
+      <Button>Default</Button>
+      <Button variant='primary'>Primary</Button>
+      <Button variant='primaryOutline'>Primary Outline</Button>
+      <Button variant='secondary'>Secondary</Button>
+      <Button variant='secondaryOutline'>Secondary</Button>
+      <Button variant='danger'>Secondary Outline</Button>
+      <Button variant='dangerOutline'>Secondary Outline</Button>
+    </div>
   );
 }
